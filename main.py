4d22fe9757362eeee578274e8902133a61808227
matsue-code <mbolamathew@icloud.com>
2025-08-08 21:04:45 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,7 @@
+for i in range(1,6,1):
+    print("bmw")
+number=int(input("enter your number"))
+sum=0
+for i in range(0,number,1):
+    sum=sum+i
+print("the sum of all number from 0 to",number,"is",sum)
